59ac41a3756d50ba7ddc96b13ca82544dd36b0ed
Rafi Imam <rafiimam96@gmail.com>
2023-11-14 03:06:09 +0600

    client home view edited

--- src/components/client-view/home/index.js
+++ src/components/client-view/home/index.js
@@ -1,8 +1,63 @@
-'use client'
+"use client";
 
-export default function ClientHomeView({data}){
+import { useMemo } from "react";
+import AnimationWrapper from "../animation-wrapper";
+import { motion } from "framer-motion";
 
-    console.log(data,'ClientHomeView');
+function variants() {
+  return {
+    offscreen: {
+      y: 150,
+      opacity: 0,
+    },
+    onscreen: ({ duration = 2 } = {}) => ({
+      y: 0,
+      opacity: 1,
+      transition: {
+        type: "spring",
+        duration,
+      },
+    }),
+  };
+}
 
-    return <div>Client Home View</div>
+export default function ClientHomeView({ data }) {
+  console.log(data, "ClientHomeView");
+
+  const setVariants = useMemo(() => variants(), []);
+
+  return (
+    <div className="max-w-screen-xl mt-24 px-8 xl:px-16 mx-auto" id="home">
+      <AnimationWrapper>
+        <motion.div
+          className={
+            "grid grid-flow-row sm:grid-flow-col grid-rows-2 md:grid-rows-1 sm:grid-cols-2 gap-8 py-6 sm:py-16"
+          }
+          variants={setVariants}
+        >
+          <div className="flex flex-col justify-center items-start row-start-2 sm:row-start-1">
+            <h1 className="mb-4 text-3xl lg:text-4xl xl:text-6xl font-medium leading-normal">
+              {data && data.length
+                ? data[0]?.heading
+                    .split(" ")
+                    .map((item, index) => (
+                      <span
+                        className={`${
+                          index === 2 || index === 3
+                            ? "text-blue-main"
+                            : "text-[black]"
+                        }`}
+                      >
+                        {item}{" "}
+                      </span>
+                    ))
+                : null}
+            </h1>
+            <p>{data && data.length
+                ? data[0]?.summary:null}</p>
+          </div>
+        </motion.div>
+      </AnimationWrapper>
+    </div>
+  );
 }
