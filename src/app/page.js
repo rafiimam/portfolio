cd8f1e025150ef7b37437ff6770da3d2446fbe02
Rafi Imam <rafiimam96@gmail.com>
2023-11-15 15:05:15 +0600

    about page updated

--- src/app/page.js
+++ src/app/page.js
@@ -4,34 +4,38 @@ import ClientExperienceAndEducationView from "@/components/client-view/experienc
 import ClientHomeView from "@/components/client-view/home";
 import ClientProjectView from "@/components/client-view/project";
 
-async function extractAllDatas(currentSection){
-  const res = await fetch(`http://localhost:3000/api/${currentSection}/get`,{
-    method: 'GET',
-    cache: 'no-store'
-  })
+async function extractAllDatas(currentSection) {
+  const res = await fetch(`http://localhost:3000/api/${currentSection}/get`, {
+    method: "GET",
+    cache: "no-store",
+  });
 
-  const data = await res.json()
+  const data = await res.json();
 
   return data && data.data;
-
 }
 
 export default async function Home() {
-
-
-  const homeSectionData = await extractAllDatas('home')
-  const aboutSectionData = await extractAllDatas('about')
-  const educationSectionData = await extractAllDatas('education')
-  const experienceSectionData = await extractAllDatas('experience')
-  const projectSectionData = await extractAllDatas('project')
+  const homeSectionData = await extractAllDatas("home");
+  const aboutSectionData = await extractAllDatas("about");
+  const educationSectionData = await extractAllDatas("education");
+  const experienceSectionData = await extractAllDatas("experience");
+  const projectSectionData = await extractAllDatas("project");
 
   return (
     <div>
-      <ClientHomeView data={homeSectionData}/>
-      <ClientAboutView data= {aboutSectionData}/>
-      <ClientExperienceAndEducationView educationData={educationSectionData} experienceData={experienceSectionData}/>
-      <ClientProjectView data={projectSectionData}/>
-      <ClientContactView/>
+      <ClientHomeView data={homeSectionData} />
+      <ClientAboutView
+        data={
+          aboutSectionData && aboutSectionData.length ? aboutSectionData[0] : []
+        }
+      />
+      <ClientExperienceAndEducationView
+        educationData={educationSectionData}
+        experienceData={experienceSectionData}
+      />
+      <ClientProjectView data={projectSectionData} />
+      <ClientContactView />
     </div>
-  )
+  );
 }
--- src/components/client-view/about/index.js
+++ src/components/client-view/about/index.js
@@ -1,5 +1,75 @@
-'use client'
+"use client";
 
-export default function ClientAboutView({data}){
-    return <div>Client About View</div>
+import { useMemo } from "react";
+import AnimationWrapper from "../animation-wrapper";
+import { motion } from "framer-motion";
+import { info } from "autoprefixer";
+
+function variants() {
+    return {
+      offscreen: {
+        y: 150,
+        opacity: 0,
+      },
+      onscreen: ({ duration = 2 } = {}) => ({
+        y: 0,
+        opacity: 1,
+        transition: {
+          type: "spring",
+          duration,
+        },
+      }),
+    };
+  }
+
+export default function ClientAboutView({ data }) {
+  console.log(data, "aboutData");
+
+  const setVariants = useMemo(() => variants(), []);
+
+  const aboutDataInfo = [
+    {
+      label: "Clients",
+      value: data?.noofclients || "0",
+    },
+    {
+      label: "Projects",
+      value: data?.noofprojects || "0",
+    },
+    {
+      label: "Experience",
+      value: data?.yearofexperience || "0",
+    },
+  ];
+
+  return (
+    <div className="max-w-screen-xl mt-24 mb-6 sm:mt-14 sm:mb-14  px-6 sm:px-8 lg:px-16 mx-auto">
+      <AnimationWrapper className="rounded-lg w-full grid-flow-row grid grid-cols-1 sm:grid-cols-3 py-9 divide-y-2 sm:divide-y-0 sm:divide-x-0 divide-blue-main bg-white-500 z-10">
+        {aboutDataInfo.map((infoItem, index) => (
+          <motion.div
+            className={`flex items-center justify-start
+                  ${
+                    index === 0
+                      ? "sm:justify-start"
+                      : index === 1
+                      ? "sm:justify-center"
+                      : "sm:justify-end"
+                  } py-4 sm:py-6 w-8/12 px-4 sm:w-auto mx-auto sm:mx-0
+
+                  `} key={index}
+                  custom={{duration: 2+index}}
+                  variants={setVariants}
+          >
+            <div className="flex m-0 w-40 sm:w-auto">
+                <div className="flex flex-col">
+                    <p className="text-[50px] text-blue-main font-bold">{infoItem.value}+</p>
+                    <p className="text-[24px] font-bold text-[#000]">{infoItem.label}</p>
+                </div>
+
+            </div>
+          </motion.div>
+        ))}
+      </AnimationWrapper>
+    </div>
+  );
 }
